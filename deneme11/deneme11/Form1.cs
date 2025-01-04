5f4b37e0d7c5d0cdfc959582c311ea9f0a1cd49f
kerem7fb <117513389+kerem7fb@users.noreply.github.com>
2025-01-04 13:29:28 +0300

    Add files via upload

---
+++ deneme11/deneme11/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+using System.Windows.Forms;
+using System.Data.OleDb;
+namespace deneme11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection yeni = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=deneme11.mdb");
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            yeni.Open();
+            string kullanici=textBox1.Text;
+            string sifre=textBox2.Text;
+            OleDbCommand komut = new OleDbCommand("Select * From Kullanici Where KullaniciAdi='" + kullanici + "'AND Sifre='" + sifre + "'", yeni);
+            OleDbDataReader oku = komut.ExecuteReader();
+            if (oku.Read())
+            {
+                MessageBox.Show("Kullanıcı Adı Ve Şifre Doğru,Giriş Başarılı");
+                Form2 ileri = new Form2();
+                ileri.Show();
+                this.Hide();
+            }
+            else 
+            {
+                MessageBox.Show("Kullanıcı Adı Veya Şifre Hatalı,Lütfen Tekrar Deneyiniz");
+            }
+            yeni.Close();
+        }
+    }
+}
---
+++ deneme11/deneme11/Form2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+namespace deneme11
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection yeni = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=deneme11.mdb");
+        public void goruntule() 
+        {
+            yeni.Open();
+            OleDbDataAdapter adtr = new OleDbDataAdapter("Select * From Bilgiler", yeni);
+            DataTable dt = new DataTable();
+            adtr.Fill(dt);
+            dataGridView1.DataSource = dt;
+            yeni.Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            goruntule();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            yeni.Open();
+            OleDbCommand komut = new OleDbCommand("Insert into Bilgiler(OgrenciNo,OgrenciAd,OgrenciSoyad,OgrenciCinsiyet)values('" + textBox1.Text + "','" + textBox2.Text + "','" + textBox3.Text + "','" + textBox4.Text + "')", yeni);
+            komut.ExecuteNonQuery();
+            yeni.Close();
+            goruntule();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            yeni.Open();
+            OleDbCommand komut = new OleDbCommand("Update Bilgiler Set OgrenciAd='" + textBox2.Text + "',OgrenciSoyad='" + textBox3.Text + "',OgrenciCinsiyet='" + textBox4.Text + "'Where OgrenciNo='" + textBox1.Text + "'", yeni);
+            komut.ExecuteNonQuery();
+            yeni.Close();
+            goruntule();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            yeni.Open();
+            OleDbCommand komut = new OleDbCommand("Delete From Bilgiler Where OgrenciNo='" + textBox1.Text + "'", yeni);
+            komut.ExecuteNonQuery();
+            yeni.Close();
+            goruntule();
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+        }
+    }
+}
